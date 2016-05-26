60a9c954657dbc2a4f0addd6b04382baf7d0a3c2
checheben <89188446@qq.com>
2016-05-26 22:28:35 +0800

    fibonacci

--- src/fibonacci_series.js
+++ src/fibonacci_series.js
@@ -1,7 +1,15 @@
 'use strict';
 
 function fibonacci_series(n) {
-
+	var arr = [];
+	for(var i=0; i<=n; i++){
+		if(i<2){
+			arr[i] = i;
+		}else{
+			arr[i] = arr[i-1]+arr[i-2];
+		}	
+	}
+	return arr;
 }
 
 module.exports = fibonacci_series;
